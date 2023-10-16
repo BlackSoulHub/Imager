3aeae8898658294eb48054da6969063c6e849aaa
BlackSoulHub <mr.asaglaev@mail.ru>
2023-10-16 20:04:10 +0300

    create settings for friend list

---
+++ Imager.Database/Configuration/FriendListEntityConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Imager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Imager.Database.Configuration;
+
+public class FriendListEntityConfiguration : IEntityTypeConfiguration<FriendListEntity>
+{
+    public void Configure(EntityTypeBuilder<FriendListEntity> builder)
+    {
+        builder.HasMany(f => f.List)
+            .WithMany();
+    }
+}
