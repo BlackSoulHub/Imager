1e387b54b013f9c700f7c2605c6250d9ee078391
BlackSoulHub <mr.asaglaev@mail.ru>
2023-10-16 20:08:33 +0300

    add referenct to services

---
+++ Imager.Web/Models/Responses/User/AuthorizationResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Imager.Web.Models.Responses.User;
+
+public class AuthorizationResponse
+{
+    [JsonPropertyName("accessToken")]
+    public required string AccessToken { get; init; }
+    [JsonPropertyName("refreshToken")]
+    public required string RefreshToken { get; init; }
+}
---
+++ Imager.Web/Models/Responses/User/GetUserImageListResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+using Imager.Domain.DTO.ImageEntity;
+
+namespace Imager.Web.Models.Responses.User;
+
+public class GetUserImageListResponse
+{
+    [JsonPropertyName("data")]
+    public required IEnumerable<ImageEntityDto> Data { get; init; }
+    [JsonPropertyName("count")]
+    public required int Count { get; init; }
+}
