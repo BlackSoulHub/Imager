b98560db932c8cae787d03ed589a7d3efb92189e
BlackSoulHub <mr.asaglaev@mail.ru>
2023-10-16 20:06:23 +0300

    create friend list entity

---
+++ Imager.Domain/Entities/FriendListEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Imager.Domain.Entities;
+
+public class FriendListEntity
+{
+    public required Guid Id { get; init; }
+    public UserEntity User { get; init; } = null!;
+    public required Guid UserId { get; init; }
+    public required List<UserEntity> List { get; set; }
+}
